aca1171e06f7c3b2d7c4c0a54d70a5f154eba160
TEGUH ATMA WIJAYA - 1106003522 <teguh.atma@bulan.cs.ui.ac.id>
2015-09-10 12:22:18 +0700

    next is [Ch5] Passing Python Var to be rendered in the template

--- lists/tests.py
+++ lists/tests.py
@@ -8,15 +8,22 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()  #1
-        response = home_page(request)  #2
-	expeted_html = render_to_string('home.html')
-	self.assertEqual(response.content.decode(), expected_html)
-        #self.assertTrue(response.content.startswith(b'<html>'))  #3
-        #self.assertIn(b'<title>To-Do lists</title>', response.content)  #4
-        #self.assertTrue(response.content.strip().endswith(b'</html>'))  #5
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()  #1
+		response = home_page(request) #2
+
+		expected_html = render_to_string('home.html')
+		self.assertEqual(response.content.decode(), expected_html)
+
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()  #1
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request) #2
+
+		self.assertIn('A new list item', response.content.decode())
--- lists/views.py
+++ lists/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-	#pass
-	#return HttpResponse('<html><title>To-Do lists</title></html>')
+	if request.method == 'POST':
+		return HttpResponse(request.POST['item_text'])
 	return render(request, 'home.html')
