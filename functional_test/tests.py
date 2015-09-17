c58f1d34ab92fe8c7ae39f9c9e9a48b7f8923a02
TEGUH ATMA WIJAYA - 1106003522 <teguh.atma@bulan.cs.ui.ac.id>
2015-09-17 10:42:40 +0700

    make functional test an app , use LiveServerTestCase

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -1,10 +1,12 @@
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+#import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+#class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -20,7 +22,8 @@ class NewVisitorTest(unittest.TestCase):
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#self.browser.get('http://localhost:8000')
-		self.browser.get('http://152.118.26.41:8522')
+		#self.browser.get('http://152.118.26.41:8522')
+		self.browser.get(self.live_server_url)
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
@@ -69,9 +72,9 @@ class NewVisitorTest(unittest.TestCase):
 # that the site has generated a unique URL for her -- there is some
 # explanatory text to that effect.
 	
-		#self.fail('Finish the test!')
+		self.fail('Finish the test!')
 
 # She visits that URL - her to-do list is still there.
 	
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+#if __name__ == '__main__':
+#	unittest.main(warnings='ignore')
