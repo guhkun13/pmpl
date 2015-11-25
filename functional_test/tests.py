33be575ae7933204ab7a5aacf607de108fe359d8
TEGUH ATMA WIJAYA - 1106003522 <teguh.atma@bulan.cs.ui.ac.id>
2015-11-25 17:19:12 +0700

    sukses membuat staging site di guhkun13.cloudapp.net

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -1,4 +1,4 @@
-
+import sys
 #from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
@@ -10,10 +10,24 @@ from selenium.webdriver.common.keys import Keys
 #class NewVisitorTest(LiveServerTestCase):
 class NewVisitorTest(StaticLiveServerTestCase):
 
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
+	
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
-
+	
 	def tearDown(self):
 		self.browser.quit()
 	
@@ -25,7 +39,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#self.browser.get('http://localhost:8000')
 		#self.browser.get('http://152.118.26.41:8522')
-		self.browser.get(self.live_server_url)
+		#self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
@@ -83,7 +98,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 # Francis visits the home page.  There is no sign of Edith's
 # list
-		self.browser.get(self.live_server_url)
+		#self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -120,7 +136,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 #	unittest.main(warnings='ignore')
 
 	def test_layout_and_styling(self):
-		self.browser.get(self.live_server_url)
+		#self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
+
 		self.browser.set_window_size(1024, 768)
 		
 		inputbox = self.browser.find_element_by_id('id_new_item')
